9f56daf644050f99ba72df4d6980006c0b704a20
sunilmondal4you <sunilmondal4you@gmail.com>
2020-04-26 00:10:36 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var x = 0;
+function myFunction() {
+    var abc = document.getElementById('myDiv').offsetHeight;
+    var pDivHt = document.getElementById('myDiv').scrollHeight;
+    var scrollHt = pDivHt*.9;
+
+    var elmnt = document.getElementById("myDiv");
+    if((abc+elmnt.scrollTop) > scrollHt){
+        alert("max Scroll Position!")
+        const div = document.createElement('div');
+        div.className = 'child2-div';
+
+        document.getElementById('myDiv').appendChild(div);
+    }
+    scrollHt = undefined;
+}
